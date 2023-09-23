8e483c761cf496bbb8373ee5dbdb3066b14eace2
Tanay Jha <tanayjha92@gmail.com>
2023-09-23 22:38:19 +0530

    Create profile

---
+++ app/src/main/java/com/example/jainshaadi/Profile.java
@@ -0,0 +1,22 @@
+package com.example.jainshaadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+     this.setTitle("Create Profile For");
+
+
+
+    }
+}
